3c16fc9c18abac76c4a9c2627d2519587e4d432d
Kirk Chen <rwk0119@yahoo.com.tw>
2016-02-03 23:15:30 +0800

    Test pull request trigger build

---
+++ SonarQubeSample/TestHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SonarQubeSample
+{
+    public class TestHelper
+    {
+        public static string SaySomething(string name)
+        {
+            Console.WriteLine("It's just a test");
+        }
+    }
+}
